210bec2840cc382c6e8e5240dc61d25cef31a270
Oliver <oliverjgodoy@gmail.com>
2021-03-25 09:43:42 -0300

    check all routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'})
-})
 
+//Rotas ->
+
+//GET
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+//POST
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+//PUT
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 7',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+//DELETE
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 7',
+    'Projeto 2',
+  ]);
+});
+
+
+
+//PORT localhost:3333
 app.listen(3333, () => {
   console.log('Start developer server')
 });
